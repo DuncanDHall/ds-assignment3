ed86e808fc933a20f2f99f90e4203efacaa12d10
Jenny Wang <Jenny Wang>
2017-03-10 09:32:56 -0500

    files

--- account_int.java
+++ account_int.java
@@ -1,9 +1,6 @@
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
-/**
- * Created by duncan on 2/10/17.
- */
 public interface Account_int extends Remote{
     //void pushMessage(String msg) throws RemoteException;
 }
