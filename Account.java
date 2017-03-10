4733c482ad22217a29555856f2a438a5d47e91a0
Duncan Hall <duncan.hall@students.olin.edu>
2017-03-10 09:46:01 -0500

    added accoutn constructor

--- Account.java
+++ Account.java
@@ -3,8 +3,18 @@ import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
 
-public class Account implements Account_int{
-    
+public class Account implements Account_int {
+
+    private int balance;
+
+    public void Account (int balance) {
+        this.balance = balance;
+    }
+
+    void transfer(int amount) throws RemoteException {
+        // TODO
+    }
+
     public static void main(String[] args) {
 
     }
--- Account_int.java
+++ Account_int.java
@@ -2,5 +2,5 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 public interface Account_int extends Remote{
-    //void pushMessage(String msg) throws RemoteException;
+    void transfer(int amount) throws RemoteException;
 }
