f2be5fd5bfcf18af4ab68db36b077f5953ec54e5
Jenny Wang <Jenny Wang>
2017-03-10 09:28:45 -0500

    initialize files

---
+++ account.java
@@ -0,0 +1,11 @@
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
+
+public class Account implements Account_int{
+    
+    public static void main(String[] args) {
+
+    }
+}
---
+++ account_int.java
@@ -0,0 +1,9 @@
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+/**
+ * Created by duncan on 2/10/17.
+ */
+public interface Account_int extends Remote{
+    //void pushMessage(String msg) throws RemoteException;
+}
