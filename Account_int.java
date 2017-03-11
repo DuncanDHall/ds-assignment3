f97eb07841d7f42333a11e031343d626a87b9878
Duncan Hall <duncan.hall@students.olin.edu>
2017-03-11 13:11:53 -0500

    adjust interface

--- Account_int.java
+++ Account_int.java
@@ -3,7 +3,13 @@ import java.rmi.RemoteException;
 
 public interface Account_int extends Remote{
 	String getID() throws RemoteException;
+
+	// transfer functionality
     void receive(int amount) throws RemoteException;
     void connect(Account_int stub) throws RemoteException;
+
+    // leader election
     void leaderIs(String accountID) throws RemoteException;
+
+    // snapshot
 }
